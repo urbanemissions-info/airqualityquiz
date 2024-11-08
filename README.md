f644058ba8052dd170c9306429b88a8fe8f1bf18
d-saikrishna <saikrishnadammalapati@gmail.com>
2024-11-08 22:01:06 +0530

    ADD new questions

--- README.md
+++ README.md
@@ -4,6 +4,11 @@
 
 <img width="400" alt="Screen Shot 2022-01-19 at 6 19 32 PM" src="https://user-images.githubusercontent.com/36372968/150233956-8550e069-05e2-4bcd-bf23-270cad6cc590.png">
 
+## Deploy on GH pages
+[Video tutorial](https://www.youtube.com/watch?v=7wzuievFjrk)
+
+
+# 
 ## Improvements
 
 ### Easy 🟢 
@@ -11,8 +16,10 @@
 - Store list of questions in a seperate file - `questions.txt`
 - Change list of questions to something else
 
-### Medium 🟡 
-- After game reset, **scramble order of questions** randomly
+### Medium 🟡
+
+- [x] After game reset, **scramble order of questions** randomly
+
 - After clicking an option, pause for 2 sec, show correct answer, then move onto next one
 - At the end show list of wrong questions with the correct answers
 - Create different ReactJS components
--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ function App() {
   const [showResults, setShowResults] = useState(false);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
+  const [begin_shuffle, setBeginShuffle] = useState(true);
 
   const questions = [
     {
+      id: 1,
       text: "What is Particulate Matter (PM) 2.5?",
       options: [
         { id: 0, text: "All PM with radius less than 2.5 µm", isCorrect: false },
@@ -19,17 +21,19 @@ function App() {
     },
 
     {
+      id: 2,
       text: "What is the main ingredient of the acid rain?",
       options: [
         { id: 0, text: "Nitrogen dioxide (NO2)", isCorrect: false },
-        { id: 1, text: "Sulphuric acid (H2SO4)", isCorrect: false },
+        { id: 1, text: "Carbon Dioxide (CO2)", isCorrect: false },
         { id: 2, text: "Carbon Monoxide (CO)", isCorrect: false },
         { id: 3, text: "Sulphur dioxide (SO2)", isCorrect: true },
       ],
     },
 
     {
-      text: "Which of the following is not a necessary ingredient in the production of Tropospheric Ozone?",
+      id: 3,
+      text: "Which of the following is not a necessary ingredient in the production of 'Global' Tropospheric Ozone?",
       options: [
         { id: 0, text: "Nitrogen Oxides (NOx)", isCorrect: false },
         { id: 1, text: "Carbon dioxide (CO2)", isCorrect: true },
@@ -39,7 +43,8 @@ function App() {
     },
 
     {
-      text: "Which of the following pollutants can exist in both gas and aerosol phases?",
+      id: 4,
+      text: "Which of the following gaseous pollutants can directly contribute to aerosol concentrations?",
       options: [
         { id: 0, text: "Particulate Matter (PM 2.5)", isCorrect: false },
         { id: 1, text: "Sulphur Dioxide (SO2)", isCorrect: true },
@@ -49,26 +54,18 @@ function App() {
     },
 
     {
+      id: 5,
       text: "What is the unit of Air Quality Index (AQI) ?",
       options: [
         { id: 0, text: "Parts per million (ppm)", isCorrect: false },
         { id: 1, text: "Micrograms (µg)", isCorrect: false },
-        { id: 2, text: "Micrograms per unit volumen (µg/m3)", isCorrect: false },
+        { id: 2, text: "Micrograms per unit volume (µg/m3)", isCorrect: false },
         { id: 3, text: "None", isCorrect: true },
       ],
     },
 
     {
-      text: "What AQI value is considered 'Good' in India?",
-      options: [
-        { id: 0, text: "0-10", isCorrect: false },
-        { id: 1, text: "50-100", isCorrect: false },
-        { id: 2, text: "0-25", isCorrect: false },
-        { id: 3, text: "0-50", isCorrect: true },
-      ],
-    },
-
-    {
+      id: 6,
       text: "What is the fundamental equation in building an Emissions Inventory?",
       options: [
         { id: 0, text: "Activity * Emissions Factor", isCorrect: true },
@@ -79,38 +76,210 @@ function App() {
     },
     
     {
+      id: 7,
       text: "What is the representative area of an air quality monitor?",
       options: [
-        { id: 0, text: "10 sq.km", isCorrect: false },
-        { id: 1, text: "4 sq.km", isCorrect: true },
-        { id: 2, text: "2 sq.km", isCorrect: false },
-        { id: 3, text: "20 sq.km", isCorrect: false },
+        { id: 0, text: "Area with a radius of 1 km", isCorrect: false },
+        { id: 1, text: "Area with a radius of 2 km", isCorrect: true },
+        { id: 2, text: "Area with a radius of 4 km", isCorrect: false },
+        { id: 3, text: "Area with a radius of 10 km", isCorrect: false },
       ],
     },
 
     {
+      id: 8,
       text: "What does source apportionment study aim to determine?",
       options: [
         { id: 0, text: "When the pollution occurred", isCorrect: false },
         { id: 1, text: "The geographical location of pollution", isCorrect: false },
         { id: 2, text: "What all activities contributed to the air pollution", isCorrect: true },
-        { id: 3, text: "The composition of pollutants", isCorrect: false },
+        { id: 3, text: "The chemical composition of pollutants", isCorrect: false },
       ],
     },
 
     {
-      text: "What action is recommended when idling in traffic for more than 30 seconds??",
+      id: 9,
+      text: "What action is recommended when idling in traffic for more than 30 seconds?",
       options: [
-        { id: 0, text: "Keep the engine running to avoid restart, which causes more pollution.", isCorrect: false },
+        { id: 0, text: "Keep the engine running to avoid restart, which causes more pollution", isCorrect: false },
         { id: 1, text: "Reduce air conditioning", isCorrect: false },
         { id: 2, text: "Turn off the engine", isCorrect: true },
         { id: 3, text: "Put the vehicle on a Neutral gear", isCorrect: false },
       ],
     },
+
+    {
+      id: 10,
+      text: "Main pollutant of volcanic eruptions is ?",
+      options: [
+        { id: 0, text: "Nitrogen Dioxide (NO2)", isCorrect: false },
+        { id: 1, text: "Sulphur Dioxide (SO2)", isCorrect: true },
+        { id: 2, text: "Ozone (O3)", isCorrect: false },
+        { id: 3, text: "Volatile Organic Compounds (VOCs)", isCorrect: false },
+      ],
+    },
+
+    {
+      id: 11,
+      text: "Pollutant that gets attached to blood (haemoglobin) and leads to premature mortality?",
+      options: [
+        { id: 0, text: "Nitrogen Dioxide (NO2)", isCorrect: false },
+        { id: 1, text: "Benzene (C6H6)", isCorrect: false },
+        { id: 2, text: "Ozone (O3)", isCorrect: false },
+        { id: 3, text: "Carbon Monoxide (CO)", isCorrect: true },
+      ],
+    },
+
+    {
+      id: 12,
+      text: "Lightning produces this in large quantities?",
+      options: [
+        { id: 0, text: "Nitrogen oxides (NOx)", isCorrect: true },
+        { id: 1, text: "Halogen compounds", isCorrect: false },
+        { id: 2, text: "Ozone (O3)", isCorrect: false },
+        { id: 3, text: "Volatile Organic Compounds (VOCs)", isCorrect: false },
+      ],
+    },
+
+    {
+      id: 13,
+      text: "Pollutant concentrations most used for calculating health impacts?",
+      options: [
+        { id: 0, text: "Sulphur Dioxide (SO2) and Nitrogen Dioxide (NO2)", isCorrect: false },
+        { id: 1, text: "Particulate Matter 2.5 (PM2.5) and Ozone (O3)", isCorrect: true },
+        { id: 2, text: "Nitrogen Dioxide (NO2) and Particulate Matter 2.5 (PM2.5)", isCorrect: false },
+        { id: 3, text: "NO2, Benzene (C6H6) and Ozone (O3)", isCorrect: false },
+      ],
+    },
+
+    {
+      id: 14,
+      text: "Good ozone protecting us from UV radiation resides in?",
+      options: [
+        { id: 0, text: "Troposphere", isCorrect: false },
+        { id: 1, text: "Heliosphere", isCorrect: false },
+        { id: 2, text: "Mesosphere", isCorrect: false },
+        { id: 3, text: "Stratosphere", isCorrect: true },
+      ],
+    },
+
+    {
+      id: 15,
+      text: "Chronic Obstructive Pulmonary Disorder (COPD) is associated with ?",
+      options: [
+        { id: 0, text: "Heart", isCorrect: false },
+        { id: 1, text: "Lungs", isCorrect: true },
+        { id: 2, text: "Eyes", isCorrect: false },
+        { id: 3, text: "Veins", isCorrect: false },
+      ],
+    },
+
+    {
+      id: 16,
+      text: "Ultrafine particulates are Particulate Matter (PM) less than?",
+      options: [
+        { id: 0, text: "10μm", isCorrect: false },
+        { id: 1, text: "1μm", isCorrect: false },
+        { id: 2, text: "0.1μm", isCorrect: true },
+        { id: 3, text: "0.01μm", isCorrect: false },
+      ],
+    },
+
+    {
+      id: 17,
+      text: "Ambient air pollution sources are?",
+      options: [
+        { id: 0, text: "Everything that burns", isCorrect: true },
+        { id: 1, text: "mostly power plants", isCorrect: false },
+        { id: 2, text: "mostly transport", isCorrect: false },
+        { id: 3, text: "waste burning", isCorrect: false },
+      ],
+    },
+
+    {
+      id: 18,
+      text: "Which of the following statements is TRUE",
+      options: [
+        { id: 0, text: "Air quality index provides more information than air quality data ", isCorrect: false },
+        { id: 1, text: "Air quality index information is preferred for regulatory purposes", isCorrect: false },
+        { id: 2, text: "Air quality index information is preferred for public awareness", isCorrect: true },
+        { id: 3, text: "Air quality index value is same as PM2.5 concentration value", isCorrect: false },
+      ],
+    },
+
+    {
+      id: 19,
+      text: "Which of the following statements is TRUE",
+      options: [
+        { id: 0, text: "Discussions on pollution is same as discussions on concentrations", isCorrect: true },
+        { id: 1, text: "Discussions on emissions is same as discussions on pollution", isCorrect: false },
+        { id: 2, text: "Discussions on emissions is same as discussions on air quality index", isCorrect: false },
+        { id: 3, text: "Discussions on pollutants is same as discussions on air quality index", isCorrect: false },
+      ],
+    },
+
+    {
+      id: 20,
+      text: "Which of the following statements is TRUE",
+      options: [
+        { id: 0, text: "Two monitoring stations is a good sample to represent a city", isCorrect: false },
+        { id: 1, text: "Low-cost sensors are same as regulatory-grade monitoring systems", isCorrect: false },
+        { id: 2, text: "Regulatory-grade systems do not require any maintenance", isCorrect: false },
+        { id: 3, text: "All monitoring systems require regular maintenance and calibration", isCorrect: true },
+      ],
+    },
+
+    {
+      id: 21,
+      text: "Which of the following statements is TRUE",
+      options: [
+        { id: 0, text: "Satellite observations can directly replace ground measurements", isCorrect: false },
+        { id: 1, text: "Satellite observations are superior to ground measurements", isCorrect: false },
+        { id: 2, text: "Satellite observations are a proxy to ground measurements", isCorrect: true },
+        { id: 3, text: "Satellite observations are a proxy to emission inventories", isCorrect: false },
+      ],
+    },
+
+    {
+      id: 22,
+      text: "What is TRUE about Aerosol Optical Depth (AOD)?",
+      options: [
+        { id: 0, text: "AOD is a proxy of PM2.5 or PM10 concentrations in the air", isCorrect: true },
+        { id: 1, text: "AOD from satellites is same as PM2.5/PM10 from ground measurements", isCorrect: false },
+        { id: 2, text: "AOD can be used for policy discussions globally", isCorrect: false },
+        { id: 3, text: "AOD from a single satellite pass per day is enough to deduce pollution", isCorrect: false },
+      ],
+    },
+
+    {
+      id: 23,
+      text: "Best way to reduce pollution",
+      options: [
+        { id: 0, text: "Increase the number of monitoring stations", isCorrect: false },
+        { id: 1, text: "Control emissions at all the sources", isCorrect: true },
+        { id: 2, text: "Use vacuum cleaners to suck pollution in the air", isCorrect: false },
+        { id: 3, text: "All the above", isCorrect: false },
+      ],
+    },
+
+    {
+      id: 24,
+      text: "Which of the following statements is TRUE about source apportionment techniques",
+      options: [
+        { id: 0, text: "Sampling-chemical-analysis approach is always preferred", isCorrect: false },
+        { id: 1, text: "Sampling-chemical-analysis approach is the best", isCorrect: false },
+        { id: 2, text: "Emissions-pollution-modelling approach is always preferred", isCorrect: false },
+        { id: 3, text: "Emissions-pollution-modelling approach has better spatial and temporal coverage ", isCorrect: true },
+      ],
+    },
+
+    
   ];
 
-  // Helper Functions
+  
+const [questionsWithRandomIds, setQuestionsWithRandomIds] = useState(questions);
 
+  // Helper Functions
   /* A possible answer was clicked */
   const optionClicked = (isCorrect) => {
     // Increment the score
@@ -118,7 +287,7 @@ function App() {
       setScore(score + 1);
     }
 
-    if (currentQuestion + 1 < questions.length) {
+    if (currentQuestion + 1 < questionsWithRandomIds.length) {
       setCurrentQuestion(currentQuestion + 1);
     } else {
       setShowResults(true);
@@ -130,7 +299,19 @@ function App() {
     setScore(0);
     setCurrentQuestion(0);
     setShowResults(false);
+    // Shuffle the questions array once (using Fisher-Yates algorithm)
+  for (let i = questions.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [questions[i], questions[j]] = [questions[j], questions[i]];
+  }
+    setQuestionsWithRandomIds(questions);
   };
+
+  if (begin_shuffle) {
+    restartGame();
+    setBeginShuffle(false);
+
+  }
 
   return (
     <div className="App">
@@ -146,23 +327,24 @@ function App() {
         <div className="final-results">
           <h1>Final Results</h1>
           <h2>
-            {score} out of {questions.length} correct - (
-            {(score / questions.length) * 100}%)
+            {score} out of {questionsWithRandomIds.length} correct - (
+            {(score / questionsWithRandomIds.length) * 100}%)
           </h2>
           <button onClick={() => restartGame()}>Restart game</button>
         </div>
       ) : (
         /* 5. Question Card  */
+        
         <div className="question-card">
           {/* Current Question  */}
           <h2>
-            Question: {currentQuestion + 1} out of {questions.length}
+            Question: {currentQuestion + 1} out of {questionsWithRandomIds.length}
           </h2>
-          <h3 className="question-text">{questions[currentQuestion].text}</h3>
+          <h3 className="question-text">{questionsWithRandomIds[currentQuestion].text}</h3>
 
           {/* List of possible answers  */}
           <ul>
-            {questions[currentQuestion].options.map((option) => {
+            {questionsWithRandomIds[currentQuestion].options.map((option) => {
               return (
                 <li
                   key={option.id}
